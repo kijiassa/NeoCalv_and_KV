1f8fdc78793bf423ac5e12472b0d544564041116
Alexey <118394879+kijiassa@users.noreply.github.com>
2022-11-21 12:18:41 +0400

    add function Button

--- Lesson/Lesson2.py
+++ Lesson/Lesson2.py
@@ -5,9 +5,18 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+
 
 class MyGrid(Widget):
-    pass
+    nameTh = ObjectProperty(None)
+    emailTh = ObjectProperty(None)
+
+    def btn(self):
+        print("Name: ", self.nameTh.text, "email: ", self.emailTh.text)
+        self.nameTh.text = ''
+        self.emailTh.text = ''
+
 
 
 class MyApp(App):
